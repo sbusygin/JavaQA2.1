3f137f870788bca24b58cde8a82d58853eaf739f
Sergey Busygin <tranebus3@yandex.ru>
2021-03-30 18:43:20 +0300

    Modify

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int price) {
+        int calculation = 20;
+        int bonusMile = price / calculation;
+
+        return bonusMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         int ticketCost = 56000;
         int calculation = 20;
 
-        int bonusMile = ticketCost / calculation;
+        int bonusMile = service.calculate(ticketCost);;
         System.out.println("Ваш билет куплен за: " + ticketCost);
         System.out.println("Вам начисленно " + bonusMile + " бонусных миль");
+
     }
 }
